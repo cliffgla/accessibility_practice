c38c8e5b3d0268adefc29311490ff6d15e669969
cliffgla <cliffordgagliardo@gmail.com>
2017-11-21 11:54:39 +0000

    removed extra space in js file

--- accessibility.js
+++ accessibility.js
@@ -1,12 +1,10 @@
-
-
 window.onload = function(){
 
     document.getElementById('button').onclick = function (){
         document.getElementById('larger').style.fontSize = "2em";
     }
+    
     document.getElementById('original').onclick = function (){
         document.getElementById('larger').style.fontSize = "1em";
     }
-
 }
